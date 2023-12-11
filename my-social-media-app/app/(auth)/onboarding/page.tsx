bf63a05a4133f96f613926495047913044f0ac24
Lingchao-Zhang <108935399+Lingchao-Zhang@users.noreply.github.com>
2023-12-11 16:50:00 +1100

    onBoarding view and func completed: (#3)
    1. created AccountProfile to implement the onboarding form using shadui.
    2. configured fileRouter using uploadthing to re-upload image when submit.
    3. provided onboarding form format validation using zod in user.ts.
    4. added a func in utils to check whether the image has changed or not.
    5. configured images hostname in next.config.js.
    6. created AccountProfileType to provide a type for the props.

--- my-social-media-app/app/(auth)/onboarding/page.tsx
+++ my-social-media-app/app/(auth)/onboarding/page.tsx
@@ -1,7 +1,31 @@
+import AccountProfile from "@/components/forms/AccountProfile"
+import { currentUser } from "@clerk/nextjs"
+
 const OnBoarding = async () => {
+    const user = await currentUser()
+    const userInfo = {
+        _id: "",
+        name: "Lingchao",
+        biography: ""
+    }
+    const userData = {
+        id: user?.id || "",
+        objectId: userInfo?._id || "",
+        username: user?.username || "",
+        name: userInfo?.name || user?.firstName || "",
+        biography: userInfo?.biography || "",
+        imageUrl: user?.imageUrl || ""
+    }
     return(
-        <main>
+        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
             <h1 className="head-text">Onboarding</h1>
+            <p className="text-light-2 mt-3 text-base-regular">Please fill the following fields to complete your profile setup.</p>
+            <section className="mt-9 bg-dark-2 p-10">
+                <AccountProfile 
+                user={userData}
+                buttonTitle="continue"            
+                />
+            </section>
         </main>
     )
 }
--- my-social-media-app/next.config.js
+++ my-social-media-app/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'uploadthing.com'                
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
